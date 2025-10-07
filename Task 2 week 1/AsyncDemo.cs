10ff2acaffa5dceb79bc29a275e4cb3276803209
Rubick1337 <poss1337@gmail.com>
2025-10-07 12:53:37 +0300

    feat: add class DataProcessor

--- Task 2 week 1/AsyncDemo.cs
+++ Task 2 week 1/AsyncDemo.cs
@@ -3,54 +3,17 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
+using Task_2_week_1;
 
 class Program
 {
-    static string ProcessDataSynchronous(string dataName)
-    {
-        Thread.Sleep(3000);
-        return $"Обработка '{dataName}' завершена за 3 секунды";
-    }
-    static async Task<string> ProcessDataAsync(string dataName)
-    {
-        await Task.Delay(3000).ConfigureAwait(false);
-        return $"Обработка '{dataName}' завершена за 3 секунды (async)";
-    }
-    static void RunSynchronusOperations()
-    {
-        string resultTaskSynchronous1 = ProcessDataSynchronous("data1");
-        Console.WriteLine(resultTaskSynchronous1);
-
-        string resultTaskSynchronous2 = ProcessDataSynchronous("data2");
-        Console.WriteLine(resultTaskSynchronous2);
-
-        string resultTaskSynchronous3 = ProcessDataSynchronous("data3");
-        Console.WriteLine(resultTaskSynchronous3);
-    }
-    static async Task RunAsyncOperations()
-    {
-        Task<string> resultTaskAsync1 = ProcessDataAsync("data1");
-
-        Task<string> resultTaskAsync2 = ProcessDataAsync("data2");
-
-        Task<string> resultTaskAsync3 = ProcessDataAsync("data3");
-
-        var tasks = new List<Task<string>> { resultTaskAsync1, resultTaskAsync2, resultTaskAsync3 };
-
-        while (tasks.Count > 0)
-        {
-            Task<string> completedTask = await Task.WhenAny(tasks);
-            tasks.Remove(completedTask);
-            Console.WriteLine(completedTask.Result);
-        }
-    }
-    
     static async Task Main(string[] args)
     {
+        var processor = new DataProcessor();
         Console.WriteLine("Cихронная обработка");
         var stopWatch = Stopwatch.StartNew();
 
-        RunSynchronusOperations();
+        processor.RunSynchronousOperations();
 
         stopWatch.Stop();
         Console.WriteLine($"Общее время: {stopWatch.Elapsed.TotalSeconds} сек");
@@ -58,7 +21,7 @@ class Program
         Console.WriteLine("Асихронная обработка");
         stopWatch.Restart();
 
-       await RunAsyncOperations();
+         await processor.RunAsyncOperations();
 
         stopWatch.Stop();
 
---
+++ Task 2 week 1/DataProcessor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_week_1
+{
+    public class DataProcessor
+    {
+        public string ProcessDataSynchronous(string dataName)
+        {
+            Thread.Sleep(3000);
+            return $"Обработка '{dataName}' завершена за 3 секунды";
+        }
+        public async Task<string> ProcessDataAsync(string dataName)
+        {
+            await Task.Delay(3000).ConfigureAwait(false);
+            return $"Обработка '{dataName}' завершена за 3 секунды (async)";
+        }
+        public void RunSynchronousOperations()
+        {
+            string result1 = ProcessDataSynchronous("data1");
+            Console.WriteLine(result1);
+
+            string result2 = ProcessDataSynchronous("data2");
+            Console.WriteLine(result2);
+
+            string result3 = ProcessDataSynchronous("data3");
+            Console.WriteLine(result3);
+        }
+        public async Task RunAsyncOperations()
+        {
+            Task<string> resultTaskAsync1 = ProcessDataAsync("data1");
+
+            Task<string> resultTaskAsync2 = ProcessDataAsync("data2");
+
+            Task<string> resultTaskAsync3 = ProcessDataAsync("data3");
+
+            var tasks = new List<Task<string>> { resultTaskAsync1, resultTaskAsync2, resultTaskAsync3 };
+
+            while (tasks.Count > 0)
+            {
+                Task<string> completedTask = await Task.WhenAny(tasks);
+                tasks.Remove(completedTask);
+                Console.WriteLine(completedTask.Result);
+            }
+        }
+    }
+}
