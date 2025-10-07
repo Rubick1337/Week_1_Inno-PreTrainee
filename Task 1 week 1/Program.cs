949e8c1541d1b0bf5a292dfc729c8877c8ec387b
Rubick1337 <poss1337@gmail.com>
2025-10-07 16:49:44 +0300

    fix: refactor code with classes

---
+++ Task 1 week 1/Program.cs
@@ -0,0 +1,34 @@
+﻿using Task_1_week_1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RunCalculator();
+    }
+
+    static void RunCalculator()
+    {
+        bool continueCalculation = true;
+
+        while (continueCalculation)
+        {
+            try
+            {
+                double firstNumber = InputValidator.GetNumberFromUser("Введите первое число:");
+                Calculator.PrintAvailableOperations();
+                char operationInput = InputValidator.GetOperationFromUser();
+                double secondNumber = InputValidator.GetNumberFromUser("Введите второе число:");
+                double result = Calculator.Calculate(firstNumber, secondNumber, operationInput);
+                Calculator.DisplayCalculationResult(firstNumber, secondNumber, operationInput, result);
+            }
+            catch (Exception ex)
+            {
+                ErrorHandler.DisplayError($"Произошла ошибка: {ex.Message}");
+            }
+
+            continueCalculation = Calculator.AskToContinue();
+        }
+    }
+
+}
